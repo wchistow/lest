4d9466074f2c478a5b1f016e6f6eec06771bd696
wchistow <wchistow@yandex.ru>
2023-04-03 14:21:33 +0300

    add function `run`

--- src/lest/__init__.py
+++ src/lest/__init__.py
@@ -1,3 +1,18 @@
-from .registerer import Registerer
+import traceback
+
+from registerer import Registerer
 
 register = Registerer()
+
+
+def run():
+    """Runs all tests and report information about it."""
+    for func in register.funcs:
+        print(f"Running [{func.__name__}]... ", end='')
+        try:
+            func()
+        except AssertionError as e:
+            print("FAILED:")
+            traceback.print_exc()
+        else:
+            print("OK")
