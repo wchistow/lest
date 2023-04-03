556d25a6a01c091e2761c1881434a76b14f4f58a
wchistow <wchistow@yandex.ru>
2023-04-03 14:36:59 +0300

    add more information about running

--- src/lest/__init__.py
+++ src/lest/__init__.py
@@ -11,12 +11,20 @@ console = Console()
 
 def run():
     """Runs all tests and report information about it."""
+    successful = 0
+    failed = 0
     for func in register.funcs:
-        print(f"Running [{func.__name__}]... ", end='')
+        print(f'Running [{func.__name__}]... ', end='')
         try:
             func()
         except AssertionError as e:
-            console.print("[red]FAILED:[/red]")
-            console.print(f"[red]{traceback.format_exc()}[/red]")
+            console.print('[red]FAILED:[/red]')
+            console.print(f'[red]{traceback.format_exc()}[/red]')
+            failed += 1
         else:
-            console.print("[green]OK[/green]")
+            console.print('[green]OK[/green]')
+            successful += 1
+
+    print(f'Run {successful + failed} tests:')
+    console.print(f'[green]   + Successful: {successful}[/green]')
+    console.print(f'[red]   + Failed: {failed}[/red]')
