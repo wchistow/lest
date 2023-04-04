ac1e28aa43d183a263686c90d38c30f68b1282e0
wchistow <wchistow@yandex.ru>
2023-04-04 13:53:36 +0300

    add information about time elapsed

--- src/lest/runner.py
+++ src/lest/runner.py
@@ -1,3 +1,4 @@
+from time import perf_counter
 import traceback
 from typing import Callable
 
@@ -12,20 +13,25 @@ class Runner:
         # information
         self.successful = 0
         self.failed = 0
+        self.elapsed = 0
 
     def run(self, funcs: list[Callable]) -> None:
         for func in funcs:
             print(f'Running [{func.__name__}]... ', end='')
+            start = perf_counter()
             try:
                 func()
             except AssertionError:
+                self.elapsed += perf_counter() - start
                 self.console.print('[red]FAILED:[/red]')
                 self.console.print(f'[red]{traceback.format_exc()}[/red]')
                 self.failed += 1
             else:
+                self.elapsed += perf_counter() - start
                 self.console.print('[green]OK[/green]')
                 self.successful += 1
 
         print(f'Run {self.successful + self.failed} tests:')
         self.console.print(f'[green]   + Successful: {self.successful}[/green]')
         self.console.print(f'[red]   + Failed: {self.failed}[/red]')
+        self.console.print('[blue]   + Time elapsed: {:5.3f}[/blue]'.format(self.elapsed))
