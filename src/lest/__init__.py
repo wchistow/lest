d1646466b379eecd3595ba592f3fce750d05aac9
wchistow <wchistow@yandex.ru>
2023-04-04 13:57:50 +0300

    add `__all__` and fix imports

--- src/lest/__init__.py
+++ src/lest/__init__.py
@@ -1,8 +1,10 @@
-from registerer import Registerer
-from runner import Runner
+from .registerer import Registerer
+from .runner import Runner
 
 registerer = Registerer()
 runner = Runner()
 
 register = registerer.__call__
 run = lambda: runner.run(registerer.funcs)
+
+__all__ = ['register', 'run']
