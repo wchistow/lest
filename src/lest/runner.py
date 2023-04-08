280611a874ea15edb8ccaa92f2219152522891a9
wchistow <wchistow@yandex.ru>
2023-04-08 10:31:39 +0300

    add results table

--- src/lest/runner.py
+++ src/lest/runner.py
@@ -3,6 +3,7 @@ import traceback
 from typing import Callable
 
 from rich.console import Console
+from rich.table import Table
 
 
 class Runner:
@@ -31,7 +32,16 @@ class Runner:
                 self.console.print('[green]OK[/green]')
                 self.successful += 1
 
-        print(f'Run {self.successful + self.failed} tests:')
-        self.console.print(f'[green]   + Successful: {self.successful}[/green]')
-        self.console.print(f'[red]   + Failed: {self.failed}[/red]')
-        self.console.print('[blue]   + Time elapsed: {:5.3f}[/blue]'.format(self.elapsed))
+        result_table = Table(title='Tests stats')
+
+        result_table.add_column('Total tests', style='blue')
+        result_table.add_column('Successful', style='green')
+        result_table.add_column('Failed', style='red')
+        result_table.add_column('Time elapsed', style='white')
+
+        result_table.add_row(str(self.successful + self.failed),
+                             str(self.successful),
+                             str(self.failed),
+                             '{:5.3f}'.format(self.elapsed))
+
+        self.console.print(result_table)
