dd1e4feb9a3a37fb77bece0f737e47ca50a88575
Vladimir Chistov <wchistow@yandex.ru>
2023-08-25 10:25:50 +0300

    accept value `'xml'` for argument `out_format` in function `lest.run`

--- docs/en/out_format.md
+++ docs/en/out_format.md
@@ -9,9 +9,27 @@ Their differences:
    as described in the section ["Customizing information quantity"](https://github.com/wchistow/lest/blob/master/docs/en/info_level.md).
  + `'json'` - information outputs in the JSON format: output is a dictionary,
    where keys - tests names, and values - dictionaries with key `"status"`,
-   which value can be one of `"ok"`, `"failed"` or `"error"`.
+   which value can be one of `"ok"`, `"failed"` or `"error"`:
+   ```json
+   {
+     "__main__.test_adding_two_and_two": {"status": "ok"},
+     ...
+   }
+   ```
    If argument [`info_level`](https://github.com/wchistow/lest/blob/master/docs/en/info_level.md) has value `'max'`,
    the key `"<result>"` adds with information about
    total tests count - key `"total"`, successful tests count - key `"successful"`,
    failed tests count - key `"failed"`, tests with errors count - key `"errors"`,
    and elapsed time - key `"time elapsed"`.
+ + `'xml'` - information outputs in the XML format like:
+   ```xml
+   <tests>
+     <test name="__main__.test_adding_two_and_two" status="ok" />
+     ...
+   </tests>
+   ```
+   If argument [`info_level`](https://github.com/wchistow/lest/blob/master/docs/ru/info_level.md) has value `'max'`,
+   the element `result` adds with information about
+   total tests count - attribute `total`, successful tests count - attribute `successful`,
+   failed tests count - attribute `failed`, tests with errors count - attribute `errors`,
+   and elapsed time - attribute `time_elapsed`.
--- src/lest/runner.py
+++ src/lest/runner.py
@@ -2,6 +2,7 @@ from json import dumps
 from time import perf_counter
 import traceback
 from typing import Callable, Literal, Any
+import xml.etree.ElementTree as ET
 
 from rich.console import Console
 from rich.table import Table
@@ -20,9 +21,11 @@ class Runner:
 
     def run(self, /, funcs: list[Callable], setup: Callable[..., Any] | None = None,
             info_level: Literal['min', 'normal', 'max'] = 'normal',
-            out_format: Literal['text', 'json'] = 'text') -> None:
+            out_format: Literal['text', 'json', 'xml'] = 'text') -> None:
         if out_format == 'json':
             print('{')
+
+        root = ET.Element('tests')
 
         for func in funcs:
             if info_level in ('normal', 'max') and out_format == 'text':
@@ -44,6 +47,9 @@ class Runner:
                         self.console.print(f'[red]{traceback.format_exc()}[/red]')
                 elif out_format == 'json':
                     func_info['status'] = 'failed'
+                elif out_format == 'xml':
+                    ET.SubElement(root, 'test', {'name': func.__module__ + '.' + func.__name__,
+                                                 'status': 'failed'})
                 self.failed += 1
             except:
                 self.elapsed += perf_counter() - start
@@ -56,6 +62,9 @@ class Runner:
                         self.console.print(f'[red]{traceback.format_exc()}[/red]')
                 elif out_format == 'json':
                     func_info['status'] = 'error'
+                elif out_format == 'xml':
+                    ET.SubElement(root, 'test', {'name': func.__module__ + '.' + func.__name__,
+                                                 'status': 'error'})
                 self.errors += 1
             else:
                 self.elapsed += perf_counter() - start
@@ -64,13 +73,16 @@ class Runner:
                         self.console.print('[green]OK[/green]')
                 elif out_format == 'json':
                     func_info['status'] = 'ok'
+                elif out_format == 'xml':
+                    ET.SubElement(root, 'test', {'name': func.__module__ + '.' + func.__name__,
+                                                 'status': 'ok'})
                 self.successful += 1
             finally:
                 if out_format == 'json':
                     if func is funcs[-1] and info_level != 'max':
-                        print(f'\t"{func.__module__}.{func.__name__}": {dumps(func_info)}')
+                        print(f'  "{func.__module__}.{func.__name__}": {dumps(func_info)}')
                     else:
-                        print(f'\t"{func.__module__}.{func.__name__}": {dumps(func_info)},')
+                        print(f'  "{func.__module__}.{func.__name__}": {dumps(func_info)},')
 
         total = self.successful + self.failed + self.errors
 
@@ -105,8 +117,18 @@ class Runner:
                     'errors': self.errors,
                     'time elapsed': self.elapsed
                 }
-                print(f'\t"<result>": {dumps(result_info)}')
+                print(f'  "<result>": {dumps(result_info)}')
             print('}')
+        elif out_format == 'xml':
+            ET.SubElement(root, 'result', {
+                    'total': str(total),
+                    'successful': str(self.successful),
+                    'failed': str(self.failed),
+                    'errors': str(self.errors),
+                    'time_elapsed': '{:5.3f}'.format(self.elapsed)
+                })
+            ET.indent(root)
+            print(ET.tostring(root, encoding='unicode'))
 
         if total != self.successful:  # some tests weren't successful
             exit(1)
